c22e4ddaaafa850527d5b3d947d915d42cbe8163
Gerald <Tu correo>
2022-10-20 16:32:44 -0500

    Modificaciones en el actitvity principal

--- app/src/main/java/com/geraldcode/findapp/MainActivity.java
+++ app/src/main/java/com/geraldcode/findapp/MainActivity.java
@@ -1,12 +1,9 @@
 package com.geraldcode.findapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-
 import com.geraldcode.findapp.Controller.Login;
 import com.geraldcode.findapp.Controller.Registro;
 
